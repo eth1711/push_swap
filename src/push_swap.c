4bd44061fbda2d67d69f9b50aab04ff1d6fc7659
Ethan Lim <ethanlim@Ethans-MacBook-Air.local>
2023-08-15 08:11:23 +0800

    move to Imac

--- src/push_swap.c
+++ src/push_swap.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ethanlim <ethanlim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 12:39:02 by etlim             #+#    #+#             */
-/*   Updated: 2023/08/09 17:45:23 by etlim            ###   ########.fr       */
+/*   Updated: 2023/08/14 20:41:23 by ethanlim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 int	main(int c, char **str)
-(
+{
 	t_list	*a;
 	t_list	*b;
 	int		i;
 
-	if (c <= 3 || !check_parse)
+	if (c < 3 || !check_parse)
 	
-)
+}
